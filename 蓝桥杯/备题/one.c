3aba55b26c1725a4f2dde4419d455871a8f2e633
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-11-21 11:49:38 +0800

    1

--- 蓝桥杯/备题/one.c
+++ 蓝桥杯/备题/one.c
@@ -1,26 +1,16 @@
 #include<stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
 void base64(char* b);
 int main()
 {
-    char a[100];
-    printf("input the flag!!!!\n");
-    scanf("%s",a);
-    if(strlen(a)!=22)
-    {
-        printf("error flag1!");
-        return;
-    }else if(a[15]!='p')
-    {
-        printf("error flag2!");
-        return;
-    }else if(a[17]!='i')
-    {
-        printf("error flag3!");
-        return; 
-    }else
-    {
-        base64(a);
-    }
+   srand(0x1548u);
+   int i=0;
+   for(i=0;i<9;i++)
+   {
+       printf("%d\n",(rand()%127));
+   }
     
 
 
