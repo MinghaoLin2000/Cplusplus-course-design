b06e1fd69dbc4db258913149b108366c097d8c09
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-11-23 21:53:02 +0800

    1

--- 蓝桥杯/枚举/10_5.cpp
+++ 蓝桥杯/枚举/10_5.cpp
@@ -1,56 +1,72 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <string.h>
 using namespace std;
-bool check(int x,int y,int z)
+
+
+
+unsigned char* RC4(unsigned char* C,unsigned char* key,int keylen,int length)
 {
-    while(x)
-    {
-        int temp=x%10;
-        if(temp==2||temp==4)
-        {
-            return false;
-        }
-        x/=10;
-    }
-    while(y)
-    {
-        int temp=y%10;
-        if(temp==2||temp==4)
-        {
-            return false;
-        }
-        y/=10;
-    }
-    while(z)
-    {
-        int temp=z%10;
-        if(temp==2||temp==4)
-        {
-            return false;
-        }
-        z/=10;
-    }
-    return true;
+ int S[256];
+ int T[256];
+
+ int  count = 0;
+count = keylen;
+
+ for(int i = 0; i < 256; i++)
+ {
+     S[i] = i;
+     int tmp = i % count;
+     T[i] = key[tmp];
+ }
+
+ int j = 0;
+
+ for(int  i = 0; i < 256; i++)
+ {
+     j = (j + S[i] + T[i]) % 256;
+     int tmp;
+     tmp = S[j];
+     S[j] = S[i];
+     S[i] = tmp;
+ }
+ 
+ 
+
+ int i;
+ i=0,j=0;
+ 
+ for(int p = 0; p < length; p++)
+ {
+
+    i = (i + 1) % 256;
+    j = (j + S[i]) % 256;
+    int tmp;
+    tmp = S[j];
+    S[j] = S[i];
+    S[i] = tmp;
+    
+    int k = S[(S[i] + S[j]) % 256];
+    C[p]=C[p]^k;
+ }
+ 
+ return C;
 }
+
+
 int main()
 {
-    int count=0;
-    for(int i=1;i<2019;i++)
-    {
-        for(int j=1;j<2019;j++)
-        {
-            if(i==j) continue;
-            for(int k=1;k<2019;k++)
-            {
-                if(i==k||j==k) continue;
-                if((i+j+k)==2019&&check(i,j,k))
-                {
-                    count++;
-                }
-            }
-        }
-    }
-    cout<<count/6<<endl;
-    return 0;
 
+    unsigned char A[1000] = { 0x99,0x65,0xF4,0x48,0x66,0x3B,0x49,0x8B};
+    unsigned char key[256] = "flag";
+    size_t  count = 0;
+    count = strlen((char*)A);
+	
+
+    unsigned char* B = RC4(A,key,4,8);
+    for(int i=0;i<8;i++)
+		printf("0x%02X,",B[i]);
+    
+
+	system("pause");
+    return 0;
 }
