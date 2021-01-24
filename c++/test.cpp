c97aca3e64db73cdc95fa0559c83364e839165b0
YenKoc <52554268+YenKoc@users.noreply.github.com>
2021-01-24 16:44:49 +0800

    1

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
+#include<string> //用c++风格字符串
 using namespace std;
-#define M 7
-#define n 8
-//常量是无法修改的
+
+//引用做函数的返回值
+//1.局部变量不可以返回引用，因为栈已经被释放了
+int& test01()
+{
+    int a=10; //局部变量存放在四区中的栈区
+    return a;
+}
+//2.静态变量可以返回引用，因为是存放在全局区，程序结束才释放
+int& test02()
+{
+    static int a=10;
+    return a;
+}
 int main()
 {  
-    //默认是double类型的
-    //默认情况下，输出一个小数，会显示出6位有效数字
-    float f1=3.14f;
-    cout<<"f1="<<f1<<endl;
-    double d1=3.14;
-    cout<<"d1="<<d1<<endl;
-    //统计float和double占用内存空间
-    //一般float4个字节，double则为8个字节
-    cout<<"float:"<<sizeof(float)<<endl;
+ 
 
-
+   
 }
